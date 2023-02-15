f7f1a3d2dff1fd7398adb08ce4370113a917e2f5
t3chnopath <josias.navasca@gmail.com>
2023-02-14 17:05:42 -0800

    Added comments

--- logger.py
+++ logger.py
@@ -4,16 +4,19 @@ import random
 from threading import Timer
  
 class logger:
+	#start csv thread every time secnds 
 	def __init__(self, directory, time):
 		self.directory = directory
 		self.file = None
 		self.arr = []
 		self.time = time
 		self._begin()
-
+	
+	#append array
 	def log(self, line):
 		self.arr.append(line)
-
+	
+	#internal logger
 	def _begin(self):
 		self.file = open(self.directory, 'a', newline='')
 		dataWriter = csv.writer(self.file)
@@ -24,11 +27,12 @@ class logger:
 		t = Timer(self.time, self._begin)
 		t.start()
 
-
+#create logger every 1 second, with csv in data directory
 Logger = logger('../data/rawData.csv', 1)
 
 while True:
-
+	
+	#read stdin for sensor data
 	for line in sys.stdin:
 		Logger.log(int(line))
 		sys.stdin.flush()	
--- sensors.py
+++ sensors.py
@@ -3,6 +3,7 @@ from random import randint
 from sys import stdout
 
 while True:
+	#spoof sensor data as random values 0 - 10
 	string = str(randint(0, 10)) + "\n"
 	stdout.write(string)
 	stdout.flush()
