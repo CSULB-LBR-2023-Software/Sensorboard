db382a6b28987896cae0f36f00e4f2d348fcce9e
T3chnopath <josias.navasca@gmail.com>
2023-02-17 03:24:54 -0800

    Removed scripts directory

--- launch.sh
+++ launch.sh
@@ -1,4 +1,4 @@
-name=../data/Data
+name=data/Data
 if [[ -e $name.csv || -L $name.csv ]] ; then
     i=1
     while [[ -e $name-$i.csv || -L $name-$i.csv ]] ; do
@@ -7,5 +7,4 @@ if [[ -e $name.csv || -L $name.csv ]] ; then
     name=$name-$i
 fi
 name=$name-$(date +'%m-%d-%Y-%H_%M_%S')
-
-python3 ../sensors.py | python3 ../logger.py "" "$name".csv "0.5"
+python3 sensors.py | python3 logger.py "" "$name".csv "0.5"
