195e7a681d2f127ea0143ce4b168b35f1c712369
T3chnopath <josias.navasca@gmail.com>
2023-02-17 01:34:51 -0800

    Added CSV date and timestamping

--- scripts/launchDebug.sh
+++ scripts/launchDebug.sh
@@ -1,4 +1,4 @@
-name=../data/rawData
+name=../data/Data
 if [[ -e $name.csv || -L $name.csv ]] ; then
     i=1
     while [[ -e $name-$i.csv || -L $name-$i.csv ]] ; do
@@ -6,5 +6,6 @@ if [[ -e $name.csv || -L $name.csv ]] ; then
     done
     name=$name-$i
 fi
+name=$name-$(date +'%m-%d-%Y-%H_%M_%S')
 
 python3 ../sensors.py | python3 ../logger.py "-d" "$name".csv "0.5"
